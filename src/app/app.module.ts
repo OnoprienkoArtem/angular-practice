6adbe60c8d27f825232e94495aaadb6bfc22ac7b
OnoprienkoArtem <o.artem.e@gmail.com>
2020-02-27 00:09:49 +0200

    practice a11y

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {A11yModule} from '@angular/cdk/a11y';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    A11yModule
   ],
   providers: [],
   bootstrap: [AppComponent]
