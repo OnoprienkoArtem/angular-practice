6bad0dbaa798715146a581a251779aca552f0b8a
OnoprienkoArtem <o.artem.e@gmail.com>
2020-02-28 21:24:41 +0200

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,10 @@
-import { HostListener, Component, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef, NgZone, OnDestroy } from '@angular/core';
-import { FocusMonitor, FocusOrigin } from '@angular/cdk/a11y';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy, AfterViewInit {
-  title = 'angular-practice';
-
-  
-
-  @ViewChild('monitored', { static: false }) monitoredEl: ElementRef<HTMLElement>;
-
-  origin = this.formatOrigin(null);
-
-  @ViewChild('link', { static: false }) link: ElementRef<HTMLElement>;
-
-  constructor(
-    public focusMonitor: FocusMonitor,
-    private _cdr: ChangeDetectorRef,
-    private _ngZone: NgZone
-  ) { }
-
-  // @ViewChild('skipRef', { static: false }) elem: ElementRef;
-
-  // @HostListener('window:keydown.tab', ['$event'])
-  // handleKeyDown(event: KeyboardEvent) {
-  //  
-
-  //   this.elem.nativeElement.focus();
-
-  //   console.log(this.elem);
-  // }
-  ngAfterViewInit() {
-    this.focusMonitor.monitor(this.monitoredEl)
-      .subscribe(origin => this._ngZone.run(() => {
-        this.origin = this.formatOrigin(origin);
-        this._cdr.markForCheck();
-      }));
-  }
-
-  ngOnDestroy() {
-    this.focusMonitor.stopMonitoring(this.monitoredEl);
-  }
-
-  formatOrigin(origin: FocusOrigin): string {
-    return origin ? origin + ' focused' : 'blurred';
-  }
-
-  skipClick() {
-    this.link.nativeElement.focus();
-  }
-
+export class AppComponent {
+  title = 'project';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {A11yModule} from '@angular/cdk/a11y';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    A11yModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
